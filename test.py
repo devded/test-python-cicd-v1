a90e6d346a89d568b074160cbf174845e1ac18ea
devded <devded@pm.me>
2023-02-28 19:21:08 +0000

    Add new file

--- test.py
+++ test.py
@@ -1,3 +1,12 @@
+from datetime import datetime
 print("Hello World! CICD")
 
 print("Hello World")
+
+data = datetime.now()
+print(data)
+
+with open('test.txt', 'w') as f:
+    f.write(str(data))
+
+
